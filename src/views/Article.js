6a2ab8bfaff2e1b5ab25dba3085e5376a075f899
Austin Gao <ag758@cornell.edu>
2021-01-24 16:44:08 -0500

    article restyling

--- src/views/Article.js
+++ src/views/Article.js
@@ -63,7 +63,7 @@ class Article extends Component {
         </div>
       </Slide>
       :
-      <div className="btn btn-primary nav-link"
+      <div className="articlebtn nav-link"
         style={{ marginTop: '0px', marginBottom: '5px' }}
         onClick={() => this.clickedShare()}>
         Share
@@ -73,28 +73,31 @@ class Article extends Component {
   render() {
     return (
       <div className="card fill-width" key={this.props.index}>
-        <img className="card-img-top img-crop noselect" data-src={this.props.imageURL ? this.props.imageURL : "https://source.unsplash.com/daily?nature"} alt="" />
+        <div className="canvas">
+          <h2 className="noselect">{this.props.title}</h2>
+          <img className="card-img-top img-crop noselect" data-src={this.props.imageURL ? this.props.imageURL : "https://source.unsplash.com/daily?city"} alt="" />
+        </div>
 
         <div className="card-body">
 
           <div className="container-fluid">
             <div className="row">
               <div className="spacer col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <h2 className="noselect">{this.props.title}</h2>
+
+              </div>
+              <div className="center spacer col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                {this.props.source && <p className="noselect bubble emphasized">{this.props.source}</p>}
+                {this.props.author && <p className="noselect bubble deemphasized">{this.props.author}</p>}
+                {this.props.publication_date && <p className="noselect bubble deemphasized">{this.props.publication_date}</p>}
               </div>
               <div className="spacer col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                {this.props.source && <p className="noselect bubble">{this.props.source}</p>}
-                {this.props.author && <p className="noselect bubble">{this.props.author}</p>}
-                {this.props.publication_date && <p className="noselect bubble">{this.props.publication_date}</p>}
-              </div>
-              <div className="spacer col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <p className="noselect card-text">{this.props.excerpt}</p>
+                <p className="excerpt noselect card-text">{this.props.excerpt}</p>
               </div>
               <div className="spacer col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 {this.showShareOption()}
               </div>
               <div className="spacer col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <a className="btn btn-primary nav-link" style={{ marginTop: '0px' }} href={this.props.url} target="_blank" rel="noopener noreferrer" role="button">Full Article</a>
+                <a className="articlebtn nav-link" style={{ marginTop: '0px' }} href={this.props.url} target="_blank" rel="noopener noreferrer" role="button">Full Article</a>
               </div>
             </div>
           </div>
